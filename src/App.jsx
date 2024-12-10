7c0bd4c02dcbeeb20712990d5327516c3ddcf899
Richard Washington <richard.washington@taxassist.co.uk>
2024-12-10 22:41:47 +0000

    Move title to variable inside the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 
 function App() {
+  const title = 'React';
 
   return (
     <>
       <div>
-        <h1>Hello React</h1>
+        <h1>{title}</h1>
       </div>
     </>
   )
