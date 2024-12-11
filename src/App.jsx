3ffe7977187519e6f398889bc0b651692759575a
Richard Washington <richard.washington@taxassist.co.uk>
2024-12-11 20:10:41 +0000

    Add all items to list

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,11 @@ function App() {
         <ul>
           {
           list.map((item) => {
-            return <li key={item.objectID}>{item.title}</li>;
+            return <li key={item.objectID}>
+            <span><a href={item.url}>{item.title}</a> </span>
+             <span>{item.author} </span>
+             <span>{item.num_comments} </span>
+             <span>{item.points} </span></li>;
           })
         }
         </ul>
