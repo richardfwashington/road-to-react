0599e26ac8051309188dfbb562564fdd84755335
Richard Washington <richard.washington@taxassist.co.uk>
2024-12-11 20:34:18 +0000

    Add prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import './App.css'
+import "./App.css";
 
 const welcome = {
-  greeting: 'Hey',
-  title: 'React'
+  greeting: "Hey",
+  title: "React",
 };
 
 const list = [
   {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
-    objectID: 0
+    objectID: 0,
   },
   {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
-    objectID: 1
-  }
+    objectID: 1,
+  },
 ];
 
 function description(title) {
@@ -32,34 +32,44 @@ function App() {
   return (
     <>
       <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
-       <Search />
+        <h1>
+          {welcome.greeting} {welcome.title}
+        </h1>
+        <Search />
         <hr />
         <List />
       </div>
     </>
-  )
+  );
 }
 
 function List() {
-  return  <ul>
-  {
-  list.map((item) => {
-    return <li key={item.objectID}>
-    <span><a href={item.url}>{item.title}</a> </span>
-     <span>{item.author} </span>
-     <span>{item.num_comments} </span>
-     <span>{item.points} </span></li>;
-  })
-}
-</ul>
+  return (
+    <ul>
+      {list.map((item) => {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>{" "}
+            </span>
+            <span>{item.author} </span>
+            <span>{item.num_comments} </span>
+            <span>{item.points} </span>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 function Search() {
-  return <div>
-  <label htmlFor="search">Search: </label>
-  <input id="search" type="text" />
-  <p>{description(welcome.title)}</p></div>
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <p>{description(welcome.title)}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
