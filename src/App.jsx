ed485ed2c9d325c62976c48bac277325131d8f5e
Richard Washington <richard.washington@taxassist.co.uk>
2024-12-12 16:20:46 +0000

    Configure ESLint to ignore missing prop types

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,30 @@ const welcome = {
   title: "React",
 };
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 function description(title) {
   return `This is a simple ${title} app that uses JSX.`;
 }
 
 const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   return (
     <>
       <div>
@@ -37,16 +37,16 @@ const App = () => {
         </h1>
         <Search />
         <hr />
-        <List />
+        <List list={stories} />
       </div>
     </>
   );
 };
 
-const List = () => {
+const List = (props) => {
   return (
     <ul>
-      {list.map((item) => {
+      {props.list.map((item) => {
         return (
           <li key={item.objectID}>
             <span>
@@ -63,10 +63,15 @@ const List = () => {
 };
 
 const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
       <p>{description(welcome.title)}</p>
     </div>
   );
