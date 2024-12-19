f1b35f827b83fce49346e964b073e3ca3e5ee139
Richard Washington <richard.washington@taxassist.co.uk>
2024-12-19 22:39:52 +0000

    Remove faked error state

--- src/App.jsx
+++ src/App.jsx
@@ -43,10 +43,6 @@ const App = () => {
     setIsLoading(true);
     getAsyncStories()
       .then((result) => {
-        // eslint-disable-next-line no-constant-condition
-        if (1 === 1) {
-          throw new Error("Error");
-        }
         setStories(result.data.stories);
         setIsLoading(false);
       })
