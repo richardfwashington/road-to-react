a9b7c89d1a6e9916b587f9fbbb7b7a8b326660db
Richard Washington <richard.washington@taxassist.co.uk>
2024-12-19 17:04:59 +0000

    Use conditional rendering to show loading / error from async data

--- src/App.jsx
+++ src/App.jsx
@@ -36,11 +36,21 @@ const getAsyncStories = () => {
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
-    getAsyncStories().then((result) => {
-      setStories(result.data.stories);
-    });
+    setIsLoading(true);
+    getAsyncStories()
+      .then((result) => {
+        // eslint-disable-next-line no-constant-condition
+        if (1 === 1) {
+          throw new Error("Error");
+        }
+        setStories(result.data.stories);
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
   }, []);
 
   const handleSearch = (event) => {
@@ -72,7 +82,12 @@ const App = () => {
         <strong>Search:</strong>
       </InputWithLabel>
       <hr />
-      <List list={searchedStories} onDelete={deleteStory} />
+      {isError && <p>Something went wrong ...</p>}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <List list={searchedStories} onDelete={deleteStory} />
+      )}
     </>
   );
 };
